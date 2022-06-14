4a839564ee4cd2abf52150c83f0ddd4da759366b
Francisco Silva <000franciscosilva000@gmail.com>
2022-06-14 11:20:35 +0100

    Added API_URL const

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,9 @@ import { App } from "./App";
 import "./index.css";
 import Login from "./pages/Login";
 
-axios.defaults.baseURL = "http://localhost:4000/";
+const API_URL = "http://api.speaq.site:4000/";
+
+axios.defaults.baseURL = API_URL;
 
 const queryClient = new QueryClient();
 
